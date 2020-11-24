cef510390eed4a5b087c3e0ace6fce8d2e367bff
Gabriel Debona <gabriel.debona@gmail.com>
2020-11-23 21:23:24 -0300

    Improve

--- web-react/src/App.js
+++ web-react/src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { useMutation, useQuery, gql } from '@apollo/client'
 
+import clsx from 'clsx'
+
 import { makeStyles } from '@material-ui/core/styles'
 
 import {
@@ -19,32 +21,43 @@ import {
   IconButton,
   Modal,
   Button,
+  Collapse,
   Backdrop,
   Fade,
   TextField,
+  CircularProgress,
 } from '@material-ui/core'
 
-import { Favorite as FavoriteIcon, Add as AddIcon } from '@material-ui/icons'
+import {
+  Favorite as FavoriteIcon,
+  Add as AddIcon,
+  ExpandMore as ExpandMoreIcon,
+  PersonAdd as PersonAddIcon,
+} from '@material-ui/icons'
 
 import { red } from '@material-ui/core/colors'
 
 // import { Link } from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 345,
-  },
   avatar: {
     backgroundColor: red[500],
   },
-  createPostButton: {
-    top: 'auto',
-    bottom: 0,
+  actionButton: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  card: {
+    width: '100%',
   },
   modal: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  topButtons: {
+    display: 'flex',
+    flexDirection: 'column',
   },
   paper: {
     backgroundColor: theme.palette.background.paper,
@@ -52,13 +65,27 @@ const useStyles = makeStyles((theme) => ({
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
 }))
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <MUILink color="inherit" href="https://grandstack.io/">
+      <MUILink color="inherit" target="_blank" href="https://www.g5l.com.br/">
         Gabriel
       </MUILink>{' '}
       {new Date().getFullYear()}
@@ -107,9 +134,11 @@ const INCREMENT_LIKE = gql`
 
 export default function App() {
   const classes = useStyles()
+
   const [open, setOpen] = React.useState(false)
   const [title, setTitle] = React.useState()
   const [description, setDescription] = React.useState()
+  const [expanded, setExpanded] = React.useState(false)
 
   const [addPost] = useMutation(ADD_POST)
   const [addLike] = useMutation(INCREMENT_LIKE)
@@ -122,48 +151,60 @@ export default function App() {
     setOpen(false)
   }
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    addPost({ variables: { title, description, numberOfLikes: 0 } })
+  const handleExpandClick = () => {
+    setExpanded(!expanded)
   }
 
-  // const incrementLikeInState = (id) => {
-  //   const post = postData.Post.find((post) => post.postId === id)
-  //   console.log({ post })
-  //   post.numberOfLikes++
-  // }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    addPost({
+      variables: { title, description, numberOfLikes: 0 },
+      refetchQueries: [{ query: GET_POSTS }],
+    })
+  }
 
   const incrementLike = ({ id, likes }) => {
     likes = likes + 1
 
     addLike({
       variables: { id, likes },
-      update: (store, { data: { UpdatePost } }) => {
-        const data = store.readQuery({ query: GET_POSTS })
-        data.Post.forEach((post) => {
-          if (post.postId === id) {
-            console.log(post.numberOfLikes, UpdatePost.numberOfLikes)
-            post.numberOfLikes = UpdatePost.numberOfLikes
-          }
-        })
-        store.writeQuery({ query: GET_POSTS, data })
-      },
+      refetchQueries: [{ query: GET_POSTS }],
     })
   }
 
   const { error, loading, data: postData } = useQuery(GET_POSTS)
 
   if (error) return <p>Error</p>
-  if (loading) return <p>Loading</p>
+  if (loading) {
+    return (
+      <Backdrop className={classes.backdrop} open={true}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    )
+  }
 
   return (
     <Container maxWidth="md">
       <CssBaseline />
-      <Grid item>
-        <Typography variant="h2" gutterBottom align="center">
-          Timeline
+      <Grid container justify="space-between" alignItems="center">
+        <Grid item>
+          <Typography variant="h2" gutterBottom align="center">
+            Timeline
+          </Typography>
+        </Grid>
+        <Grid item className={classes.topButtons}>
           <Button
-            className={classes.createPostButton}
+            className={classes.actionButton}
+            onClick={handleOpen}
+            variant="contained"
+            color="primary"
+            size="small"
+            startIcon={<PersonAddIcon />}
+          >
+            Include User
+          </Button>
+          <Button
+            className={classes.actionButton}
             onClick={handleOpen}
             variant="contained"
             color="primary"
@@ -172,12 +213,12 @@ export default function App() {
           >
             Create Post
           </Button>
-        </Typography>
+        </Grid>
       </Grid>
       <Grid container spacing={4}>
         {postData.Post.map((post, index) => (
-          <Grid key={index} item md={6} sm={1}>
-            <Card className={classes.card}>
+          <Grid key={index} item md={6} sm={1} className={classes.card}>
+            <Card>
               <CardHeader
                 avatar={
                   <Avatar aria-label="recipe" className={classes.avatar}>
@@ -205,7 +246,50 @@ export default function App() {
                   <FavoriteIcon />
                 </IconButton>
                 {post.numberOfLikes}
+                <IconButton
+                  className={clsx(classes.expand, {
+                    [classes.expandOpen]: expanded,
+                  })}
+                  onClick={handleExpandClick}
+                  aria-expanded={expanded}
+                  aria-label="show more"
+                >
+                  <ExpandMoreIcon />
+                </IconButton>
               </CardActions>
+              <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                  <Typography paragraph>Method:</Typography>
+                  <Typography paragraph>
+                    Heat 1/2 cup of the broth in a pot until simmering, add
+                    saffron and set aside for 10 minutes.
+                  </Typography>
+                  <Typography paragraph>
+                    Heat oil in a (14- to 16-inch) paella pan or a large, deep
+                    skillet over medium-high heat. Add chicken, shrimp and
+                    chorizo, and cook, stirring occasionally until lightly
+                    browned, 6 to 8 minutes. Transfer shrimp to a large plate
+                    and set aside, leaving chicken and chorizo in the pan. Add
+                    pimentón, bay leaves, garlic, tomatoes, onion, salt and
+                    pepper, and cook, stirring often until thickened and
+                    fragrant, about 10 minutes. Add saffron broth and remaining
+                    4 1/2 cups chicken broth; bring to a boil.
+                  </Typography>
+                  <Typography paragraph>
+                    Add rice and stir very gently to distribute. Top with
+                    artichokes and peppers, and cook without stirring, until
+                    most of the liquid is absorbed, 15 to 18 minutes. Reduce
+                    heat to medium-low, add reserved shrimp and mussels, tucking
+                    them down into the rice, and cook again without stirring,
+                    until mussels have opened and rice is just tender, 5 to 7
+                    minutes more. (Discard any mussels that don’t open.)
+                  </Typography>
+                  <Typography>
+                    Set aside off of the heat to let rest for 10 minutes, and
+                    then serve.
+                  </Typography>
+                </CardContent>
+              </Collapse>
             </Card>
           </Grid>
         ))}
@@ -229,27 +313,34 @@ export default function App() {
         <Fade in={open}>
           <div className={classes.paper}>
             <h2 id="transition-modal-title">Create Post</h2>
-            <form
-              className={classes.root}
-              noValidate
-              autoComplete="off"
-              onSubmit={handleSubmit}
-            >
-              <TextField
-                required
-                id="standard-required"
-                label="Required"
-                onChange={(e) => setTitle(e.target.value)}
-              />
-              <TextField
-                required
-                id="standard-required"
-                label="Required"
-                onChange={(e) => setDescription(e.target.value)}
-              />
-              <Button type="submit" variant="contained" color="primary">
-                Create
-              </Button>
+            <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+              <Grid container spacing={1}>
+                <Grid item md={12}>
+                  <TextField
+                    label="Title"
+                    required
+                    fullWidth={true}
+                    id="standard-required"
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                </Grid>
+                <Grid item md={12}>
+                  <TextField
+                    label="Description"
+                    required
+                    multiline
+                    fullWidth={true}
+                    rows={4}
+                    id="standard-required"
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
+                </Grid>
+                <Grid item md={12}>
+                  <Button type="submit" variant="contained" color="primary">
+                    Create
+                  </Button>
+                </Grid>
+              </Grid>
             </form>
           </div>
         </Fade>
